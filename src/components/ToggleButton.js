2e4078b62267446d8e0195411ff3455f6b72e150
fam246 <ola.fam10@gmail.com>
2018-06-21 23:37:33 +0100

    Toggle button

---
+++ src/components/ToggleButton.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import SearchFilter from './SearchFilter';
+
+ class ToggleButton extends Component {
+   constructor(props){
+       super(props);
+       this.state = {
+           visible: false,
+       }
+   }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+export default ToggleButton;
